c3ca3cc6772029d76f196f33ee36897b5658b562
João Campos <joaon.c.jv@gmail.com>
2026-01-13 14:05:42 -0300

    fix: ajusting landing page layout

--- src/pages/ChoosePokemon.tsx
+++ src/pages/ChoosePokemon.tsx
@@ -21,7 +21,7 @@ const ChoosePokemon = () => {
 
   return (
     <main className="flex flex-col h-screen max-lg:h-full text-white">
-      <section className="relative bg-linear-to-br from-gd-orange to-gd-blue flex items-center justify-center w-full">
+      <section className="relative bg-linear-to-br from-orange-300 to-red-900 flex items-center justify-center w-full">
         <div className="flex flex-col gap-8 items-center justify-center p-8 max-w-150 max-lg:max-w-120">
           <h1 className="text-3xl text-center font-extrabold">
             Escolha seu pokemon inicial
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,8 +6,8 @@ import Button from "../components/Button";
 
 const LandingPage = () => {
   return (
-    <main className="flex max-lg:flex-col justify-between items-center h-screen max-lg:h-full text-white">
-      <section className="relative bg-linear-to-br from-gd-orange to-gd-blue flex-2 max-lg:flex-none flex items-center justify-center h-screen max-lg:h-130 w-full max-lg:overflow-x-hidden">
+    <main className="relative bg-linear-to-tl from-gray-800 to-gray-950 flex max-lg:flex-col justify-between items-center h-full max-lg:h-full text-white overflow-x-hidden">
+      <section className=" flex-2 max-lg:flex-none flex items-center justify-center h-screen max-lg:h-130 w-full max-lg:overflow-x-hidden">
         <img
           src={bgPokeball}
           alt="pokeball"
@@ -16,24 +16,24 @@ const LandingPage = () => {
         <img
           src={charizardImage}
           alt="charizard"
-          className="absolute top-0 left-0 w-100 max-lg:w-50"
+          className="absolute top-0 left-0 w-60 max-lg:w-50"
         />
         <img
           src={ivysaurImage}
           alt="ivysaur"
-          className="absolute bottom-0 -right-25 max-lg:-right-10 w-80 max-lg:w-50 z-50"
+          className="absolute bottom-0 max-lg:top-85 -right-10 max-lg:-right-10 w-50 max-lg:w-50 z-50"
         />
         <div className="flex flex-col gap-8 items-center justify-center p-8 max-w-150 max-lg:max-w-100">
-          <h1 className="text-6xl font-extrabold">POKEGYM</h1>
+          <h1 className="text-5xl pt-8 font-extrabold">POKEGYM</h1>
           <p className="text-xl font-medium text-center">
             Se aventure nessa jormada! Seja produtivo e veja seus pokemons
             evoluirem junto com você.
           </p>
         </div>
       </section>
-      <section className="flex-3 max-lg:flex-none flex flex-col gap-12 items-center justify-center bg-bg-blue p-12 h-full w-full z-0">
+      <section className="flex-3 max-lg:flex-none flex flex-col gap-8 items-start max-lg:items-center p-12 max-lg:p-4 h-full w-full z-0">
         <h2 className="text-2xl font-extrabold">TUTORIAL</h2>
-        <div className="flex flex-col gap-4 max-w-200 text-xl">
+        <div className="flex flex-col gap-4 max-w-150 text-sm">
           <p>
             1 - Inicialmente você escolhe um pokemon para compor seu time
             inicial.
@@ -46,23 +46,21 @@ const LandingPage = () => {
 
           <p>
             3 - Após concluir todas tarefas do dia, seu pokémon escolhido vai
-            ganhar experiência (1 HP). Assim, ele vai ganhando níveis e com o
-            tempo podem evoluir para sua segunda e terceira forma.{" "}
+            ficar mais forte. Assim, ele vai ganhando níveis e com o
+            tempo podem evoluir para sua segunda e terceira forma.
           </p>
 
           <p>
             4 - Depois de evoluir seu pokemon, você receberá 3 pokebolas para
-            capturar até 3 novos pokémons na batalha. São 4 níveis de batalha a
+            capturar até 3 novos pokémons. Para capturar um novo pokémon você deve ganhar uma batalha contra ele. São 4 níveis de batalha a
             depender a força do pokémon. Caso não consiga ganhar, você pode
-            continuar treinando seu pokémon até receber mais pokebolas e
-            participar de novas batalhas.
+            continuar treinando seu pokémon até receber mais pokebolas para tentar novamente.
           </p>
 
           <p>
             Dica: pokémon num nível mais alto batalhando e ter mais pokemons
             capturados, tem mais chances de ganhar as batalhas! Além disso, você
             pode escolher pokémons mais fracos selecionando o filtro - fácil.
-            Estes, são mais facilmente capturáveis.
           </p>
 
           <p>
@@ -73,12 +71,6 @@ const LandingPage = () => {
           <p>
             6 - O pokémon que não evoluiu para sua terceira forma, se não for
             treinado por vários dias consecutivos, pode ir baixando de nível...
-          </p>
-
-          <p>
-            Dica: Evite treinar vários pokémons ao mesmo tempo, que não estejam
-            na sua forma mais evoluida, para não perder experiencia caso esqueça
-            de treinar um.
           </p>
         </div>
         <Button text="Começar" path="/choose-pokemon" />
