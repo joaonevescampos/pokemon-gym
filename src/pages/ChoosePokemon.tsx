07f85387e4cce8bc04c23a4a60053dea109b4afe
João Campos <joaon.c.jv@gmail.com>
2026-01-13 14:57:07 -0300

    fix: rebranding the ChoosePokemon page

--- src/pages/ChoosePokemon.tsx
+++ src/pages/ChoosePokemon.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import PokemonCard from "../components/PokemonCard";
+import pokebola from "../assets/pokeball.png";
+import { Link } from "react-router-dom";
 
 const initialPokemons = [
   { name: "pichu", selected: false },
@@ -16,41 +18,56 @@ const ChoosePokemon = () => {
     const oneSelected: boolean = initialPokemons.some(
       (pokemon) => pokemon.selected === true
     );
-    setInative(!oneSelected)
+    setInative(!oneSelected);
   }, [initialPokemons]);
 
   return (
-    <main className="flex flex-col h-screen max-lg:h-full text-white">
-      <section className="relative bg-linear-to-br from-orange-300 to-red-900 flex items-center justify-center w-full">
-        <div className="flex flex-col gap-8 items-center justify-center p-8 max-w-150 max-lg:max-w-120">
-          <h1 className="text-3xl text-center font-extrabold">
-            Escolha seu pokemon inicial
-          </h1>
-          <p className="text-xl font-medium text-center">
-            Escolha com sabedoria, ele vai seguir com você um bom tempo!
-          </p>
+    <>
+      <header className="relative w-full h-12 text-white">
+        <div className="absolute flex items-end gap-2 top-4 left-4">
+          <Link to="/" className="text-sm  font-bold opacity-70">
+            voltar
+          </Link>
         </div>
-      </section>
-      <section className="flex flex-col gap-12 items-center justify-center bg-bg-blue px-4 py-16 h-full">
-        <div className="grid grid-cols-4 max-lg:grid-cols-2 gap-4 items-center justify-center text-xl h-full max-w-300 m-auto">
-          {initialPokemons.map((pokemon, index) => (
-            <PokemonCard
-              key={index}
-              name={pokemon.name}
-              pokemonSelected={pokemon.selected ? pokemon.name : ""}
-            />
-          ))}
+        <div className="absolute flex items-end gap-2 top-4 right-4">
+          <span className="text-sm font-bold opacity-70">x 0 </span>
+          <img src={pokebola} alt="pokebola" width={28} />
         </div>
-        <div className="flex flex-col items-center justify-center gap-4">
-          <Button path="/my-pokemons" text="próximo" inative={inative} />
-          {inative && (
-            <p className="text-red-300 text-sm">
-              Selecione um pokemon para prosseguir!
-            </p>
-          )}
+      </header>
+      <main className="flex flex-col items-center justify-center h-screen max-lg:h-full text-white">
+        <div className="flex flex-col gap-4">
+          <section className="relative flex items-center justify-center w-full h-full pt-4">
+            <div className="flex flex-col gap-4 items-center justify-center p-4 max-w-150 max-lg:max-w-120">
+              <h1 className="text-2xl text-center font-extrabold">
+                Escolha seu pokemon inicial
+              </h1>
+              <p className="text-sm font-medium text-center">
+                Escolha com sabedoria, ele vai seguir com você um bom tempo!
+              </p>
+            </div>
+          </section>
+          <section className="flex flex-col gap-8 items-center justify-center px-4 py-8 h-full">
+            <div className="grid grid-cols-4 max-lg:grid-cols-2 gap-4 items-center justify-center text-xl h-full max-w-300 m-auto">
+              {initialPokemons.map((pokemon, index) => (
+                <PokemonCard
+                  key={index}
+                  name={pokemon.name}
+                  pokemonSelected={pokemon.selected ? pokemon.name : ""}
+                />
+              ))}
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <Button path="/my-pokemons" text="próximo" inative={inative} />
+              {inative && (
+                <p className="text-red-300 text-sm">
+                  Selecione um pokemon para prosseguir!
+                </p>
+              )}
+            </div>
+          </section>
         </div>
-      </section>
-    </main>
+      </main>
+    </>
   );
 };
 
