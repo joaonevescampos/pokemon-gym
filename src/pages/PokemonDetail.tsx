df75aa8500c83c8f81e30bbf1bbe8990d44406bc
João Campos <joaon.c.jv@gmail.com>
2026-01-15 13:16:47 -0300

    fix: redirect finish training

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Button from "../components/Button";
 import { usePokemon } from "../context/usePokemon";
 
